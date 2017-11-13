7022930d90ddf1f1fe188039e8e321973bcad45a
Fabio Sacco <saccogf@gmail.com>
2017-11-13 16:44:02 -0200

    model for core.courses
    Create model for core.courses

--- code/simplemooc/simplemooc/courses/models.py
+++ code/simplemooc/simplemooc/courses/models.py
@@ -5,4 +5,10 @@ from django.db import models
 
 class Course(models.Model):
 
-    pass
+    name = models.CharField('Name', max_length=100)
+    slug = models.SlugField('Shortcurt')
+    description = models.TextField('Description', blank=True)
+    start_date = models.DateField('Start Date', null=True, blank=True)
+    image = models.ImageField(upload_to='courses/images', verbose_name='Image')
+    created_at = models.DateTimeField('Created at', auto_now_add=True)
+    updated_at = models.DateTimeField('Updated at', auto_now=True)
