833ce2abedb586ef346442a7acb8b13ee4036b89
Fabio Sacco <saccogf@gmail.com>
2017-11-13 23:55:47 -0200

    courses/index.html
    Edits courses/index.html to fetch course data from DB

--- code/simplemooc/simplemooc/courses/views.py
+++ code/simplemooc/simplemooc/courses/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Course
 
 
 def index(request):
+    courses = Course.objects.all()
+    context = {
+        'courses': courses
+    }
     template_name = 'courses/index.html'
-    return render(request, template_name)
+    return render(request, template_name, context)
