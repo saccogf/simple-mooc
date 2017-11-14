44a808a70a1dda492b35ada7e7d8f77489ac987d
Fabio Sacco <saccogf@gmail.com>
2017-11-14 13:10:05 -0200

    email validation
    Adds email validation to register form

--- code/simplemooc/simplemooc/accounts/urls.py
+++ code/simplemooc/simplemooc/accounts/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^login/$', views.login,
         {'template_name': 'accounts/login.html'}, name='login'),
-    url(r'^register/$', accviews.register,
-        {'template_name': 'accounts/register.html'}, name='register'),
+    url(r'^register/$', accviews.register, name='register'),
+    url(r'^logout/$', accviews.logout, name='logout')
 ]
--- code/simplemooc/simplemooc/accounts/views.py
+++ code/simplemooc/simplemooc/accounts/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
+from django.conf import settings
 
 # Create your views here.
 
 
 def register(request):
     template_name = 'accounts/register.html'
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = RegisterForm()
+    context = {
+        'form': form
+    }
+    return render(request, template_name, context)
+
+
+def logout(request):
+    template_name = 'accounts/logout.html'
     return render(request, template_name)
